c8b45006eb7d13b44c308e8643e6c8445500b266
Imran Juma <imranjuma49@gmail.com>
2018-04-13 16:22:42 -0400

    Fiex Bugs
    Started coding out application and Xcode started to throw a bunch of errors with the ViewController but those are now fixed

--- ClockTime/ClockTime/ViewController.swift
+++ ClockTime/ClockTime/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Lable outlet added so it can be later changed
+    @IBOutlet weak var lable: UILabel!
+    
+    //Adding in varible so we can add different customization feautures for the user
+    var timer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Function to update timer
+    func upadteTimer() {
+        
+        //Creating the formation
+        let timeFormatter = DateFormatter()
+        
+        //Showing the tyme style
+        timeFormatter.timeStyle = .medium
+        
+        //Update the lable to show the ucrrent date for the user, this is local for wherever the user is
+        lable.text = timeFormatter.string(from: Date())
+        
+    }
 }
 
