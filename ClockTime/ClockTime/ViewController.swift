23be46c14ba119c5d7484060719ca54b738af93d
Imran Juma <imranjuma49@gmail.com>
2018-04-13 16:37:28 -0400

    More UI
    Added a few more UI elements including a secondary view, a settings button, as well as some controls for the user to play around with within the enovirment

--- ClockTime/ClockTime/ViewController.swift
+++ ClockTime/ClockTime/ViewController.swift
@@ -20,11 +20,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //update the timer as soon as the application has been loaded
+        self.updateTimer()
+        
+        //Allow the time to increase every second
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Update the timer function whenever the function is triggered
+    @objc func updateTimer() {
+        
     }
     
     // Function to update timer
@@ -41,4 +52,5 @@ class ViewController: UIViewController {
         
     }
 }
+
 
